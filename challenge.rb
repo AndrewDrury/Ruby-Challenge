d7e2aba25f505d93f2414aec89fbf74678721880
Andrew Drury <drurya99@gmail.com>
2024-10-20 17:05:40 -0400

    read jsons and output user balances

--- challenge.rb
+++ challenge.rb
@@ -1,17 +1,37 @@
 #!/usr/bin/env ruby
 
-# Load companies from JSON file
-# Load users from JSON file
+require 'json'
 
-# for each company (sorted by id):
 
-    # for each active user in company (sorted by last name):
-        # calculate new token balance
-        # create user output
-        # add user to emailed or non-emailed list
+begin
+    # Load companies from JSON file
+    companies = JSON.parse(File.read('companies.json'))
+    # Load users from JSON file
+    users = JSON.parse(File.read('users.json'))
 
-    # calculate total top-up for company
-    # create company output
-    # add company output to overall output
+    # for each company (sorted by id):
+    companies.sort_by { |c| c['id'] }.each do |company|
+        # for each active user in company (sorted by last name):
+        users.select { |u| u['company_id'] == company['id'] && u['active_status'] }
+            .sort_by { |u| u['last_name'] }
+            .each do |user|
+            
+            # calculate new token balance
+            prev_balance = user['tokens']
+            new_balance = prev_balance + company['top_up']
 
-# Write output to file
+            # create user output
+            puts "#{user['last_name']}, #{user['first_name']}, #{user['email']}"
+            puts "Previous Token Balance, #{prev_balance}"
+            puts "New Token Balance #{new_balance}\n"
+
+            # add user to emailed or non-emailed list
+            # calculate total top-up for company
+        end
+
+        # create company output
+        puts "Company #{company['name']}"
+        # add company output to overall output
+    end
+    # Write output to file
+end
