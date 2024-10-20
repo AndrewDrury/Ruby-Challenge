1c2c8e4f21ad81ba7b43264ba330d7c10bcc3e25
Andrew Drury <drurya99@gmail.com>
2024-10-20 16:52:56 -0400

    create pseudocode for challenge script

---
+++ challenge.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+# Load companies from JSON file
+# Load users from JSON file
+
+# for each company (sorted by id):
+
+    # for each active user in company (sorted by last name):
+        # calculate new token balance
+        # create user output
+        # add user to emailed or non-emailed list
+
+    # calculate total top-up for company
+    # create company output
+    # add company output to overall output
+
+# Write output to file
